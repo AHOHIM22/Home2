28f0994653ea720cddfd36dd661820a32d9d0cb8
fyjybv22226 <aho2421him@gmail.com>
2022-03-04 14:59:02 +0300

    Job

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int j = 1; j <= 100; j++) {
+            System.out.print(j+" ");
+        }
+        System.out.println();
+        for (int i = 100; i >=1 ; i--) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        for (int i = 2; i <=100 ; i=i+2) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        for (int i = 1; i < 100; i=i+2) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        for (int i = 10; i <=100 ; i=i+10) {
+            System.out.print(i+" ");
+        }
+
+
+    }
+}
